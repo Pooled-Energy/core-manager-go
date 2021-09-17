9fcc40c72c3383e5ed381b8f6477dbefd84afb4d
Rhys Johns <rhysjohns@pooledenergy.com>
2021-09-17 13:40:24 +1000

    - (Actually) implemented configure modem

--- modem.go
+++ modem.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strings"
+	"time"
 
 	"go.uber.org/zap"
 )
@@ -138,33 +139,155 @@ func (m *Modem) ConfigureApn() error {
 }
 
 func (m *Modem) ConfigureModem() error {
-	/*
-		forceReset := 0
-		zap.S().Info("modem configuration started")
+	forceReset := 0
+	zap.S().Info("modem configuration started")
 
-		err := m.ConfigureApn()
+	err := m.ConfigureApn()
+	if err != nil {
+		return err
+	}
+
+	zap.S().Info("checking modem mode...")
+	ecmMode, err := RunModemManagerCommand(m.ModeStatusCommand)
+	if err != nil {
+		return fmt.Errorf("unable to get modem mode, error: %v", err)
+	}
+
+	if strings.Contains(ecmMode, m.EcmModeResponse) {
+		zap.S().Info("ecm mode already set, skipping...")
+		return nil
+	}
+
+	zap.S().Info("modem mode not set. ECM mode will be activated")
+	_, err = RunModemManagerCommand(m.EcmModeSetterCommand)
+	if err != nil {
+		return fmt.Errorf("an issue occured when setting ECM mode")
+	}
+
+	zap.S().Info("ECM mode set, modem will reboot to apply changes")
+	time.Sleep(20 * time.Second)
+	err = checkModemStarted(m)
+	if err != nil {
+		zap.S().Error("issue restarting modem, error: %v", err)
+		forceReset = 1
+	}
+
+	if forceReset == 1 {
+		forceReset = 0
+		err := softModemReset(m)
 		if err != nil {
-			return err
+			return fmt.Errorf("an issue occured when configuring the modem, error: %v", err)
 		}
+	}
 
-		zap.S().Info("checking modem mode")
-		conn, err := dbus.ConnectSystemBus()
+	return nil
+}
+
+func checkModemStarted(modem *Modem) error {
+	result := 0
+	counter := 0
+
+	for i := 0; i < 120; i++ {
+		output, err := RunShellCommand("lsusb")
 		if err != nil {
-			return err
+			zap.S().Error("error trying to get modem information, error: %v", err)
 		}
-		defer conn.Close()
+		if strings.Contains(output, modem.Vendor) {
+			zap.S().Info("modem detected")
+			counter = 0
+			result += 1
+			break
+		} else {
+			time.Sleep(1 * time.Second)
+			counter += 1
+			fmt.Println(string(counter) + " attempts to get modem name")
+		}
+	}
 
-		var result string
-		dbusObject := conn.Object("org.freedesktop.ModemManager1", "/org/freedesktop/ModemManager1/Modem/0")
-		dBusMethodCallResult := dbusObject.Call("org.freedesktop.ModemManager1.Modem.Command", 0, m.ModeStatusCommand, uint32(30)).Store(result)
-		if dBusMethodCallResult != nil {
-			return fmt.Errorf("unable to determine modem status mdoe, response: %s", result)
+	for i := 0; i < 10; i++ {
+		output, err := RunModemManagerCommand("AT")
+		if err != nil {
+			zap.S().Error("error trying to get modem information, error: %v", err)
+		}
+		if strings.Contains(output, "OK") {
+			zap.S().Info("modem AT FW is working")
+			counter = 0
+			result += 1
+			break
+		} else {
+			time.Sleep(1 * time.Second)
+			counter += 1
+			fmt.Println(string(counter) + " attempts to get contact modem")
+		}
+	}
+
+	for i := 0; i < 20; i++ {
+		output, err := RunShellCommand("route -n")
+		if err != nil {
+			zap.S().Error("error trying to get modem information, error: %v", err)
+		}
+		if strings.Contains(output, modem.InterfaceName) {
+			zap.S().Info("modem started")
+			counter = 0
+			result += 1
+			break
+		} else {
+			time.Sleep(1 * time.Second)
+			counter += 1
+			fmt.Println(string(counter) + " attempts to get ensure modem is up")
+		}
+	}
+
+	if result != 3 {
+		return fmt.Errorf("modem could not be started")
+	}
+
+	return nil
+
+}
+
+func checkModemTurnedOff(modem *Modem) error {
+	counter := 0
+	for i := 0; i < 20; i++ {
+		output, err := RunShellCommand("lsusb")
+		if err != nil {
+			zap.S().Error("error trying to get modem information, error: %v", err)
 		}
 
-		if strings.Contains(result, m.EcmModeResponse) {
-
+		if strings.Contains(output, modem.Vendor) {
+			time.Sleep(1 * time.Second)
+			counter++
+			fmt.Println(string(counter) + " attempts to check modem is off")
+		} else {
+			zap.S().Info("modem turned off")
+			counter = 0
+			return nil
 		}
-	*/
+	}
+
+	return fmt.Errorf("modem didn't turn off as expected")
+}
+
+func softModemReset(modem *Modem) error {
+	zap.S().Info("resetting modem softly")
+	output, err := RunModemManagerCommand(modem.RebootCommand)
+	if err != nil {
+		return fmt.Errorf("unable to execute modem manager reboot command, error: %v", err)
+	}
+
+	if !strings.Contains(output, "OK") {
+		return fmt.Errorf("reboot command unable to reach modem, error: %v", err)
+	}
+
+	err = checkModemTurnedOff(modem)
+	if err != nil {
+		return err
+	}
+
+	err = checkModemStarted(modem)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
