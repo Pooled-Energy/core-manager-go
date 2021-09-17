260a3915e11fea95cf91ce93184b5fdd785ee3a0
Rhys Johns <rhysjohns@pooledenergy.com>
2021-09-17 12:45:46 +1000

    - Cleaned up connection manager with out new hardware profiler class and learnings!

--- connectionmanager.go
+++ connectionmanager.go
@@ -1,12 +1,6 @@
 package main
 
-import "fmt"
-
-var conductor ModemConductor
-
-func connectionManagerTest() {
-	fmt.Println("Hello, Ru!")
-}
+import "go.uber.org/zap"
 
 func organizer() {
 	if conductor.Base == 0 {
@@ -28,7 +22,33 @@ func organizer() {
 }
 
 func identifySetup() {
-	
+	conductor.SetStep(0, 1, 2, 15, 2, false, 20)
+
+	newId, err := GetHardwareProfile()
+	if err != nil {
+		zap.S().Error("issue occured when identifying setup, error: %v", err)
+	}
+
+	if newId != nil {
+		networkModem.Update(newId.ModemVendor, newId.ModemName, newId.IMEI,
+			newId.ICCID, newId.SoftwareVersion, newId.ModemVendorId, newId.ModemProductId)
+
+		conductor.IsOk = true
+
+		if config.DebugMode && config.VerboseMode {
+			zap.S().Info("")
+			zap.S().Info("=============================================================")
+			zap.S().Info("[?] Modem Report")
+			zap.S().Info("---------------------------")
+			zap.S().Info("%v+", networkModem)
+			zap.S().Info("=============================================================")
+			zap.S().Info("")
+		}
+	}
+}
+
+func configureModem() {
+	conductor.SetStep(0, 1, 2, 15, 2, false, 20)
 }
 
 var actions = [...]func(){organizer}
